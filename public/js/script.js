ff15c2321ad6fb3f9e3971ca076c9adcc7bcd497
Stephen <stephen.germain@free.Fr>
2020-09-23 22:18:33 +0200

    migration to symfony

---
+++ public/js/script.js
@@ -0,0 +1,49 @@
+//animation for scrollTop
+$(function()
+{
+     $(".test, #presentation a, footer a").on("click", function(event)
+     {
+            event.preventDefault();
+            var hash = this.hash;
+            $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+     });
+})
+
+
+//function animation(arg){
+	//$('arg').css('visibility', 'visible');
+	//$('arg').addClass('animate__animated animate__fadeInUp');
+//};
+//var about = document.getElementsByClassName(about-animation);
+
+// display animation
+$(function(){
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 350 && window.matchMedia('(min-width: 1200px)').matches){
+			$('.about-animation').css('visibility', 'visible');
+			$('.about-animation').addClass('animate__animated animate__fadeInUp');
+			window.setTimeout(function(){
+				$('.about-animation p').css('visibility', 'visible');
+				$('.about-animation p').addClass('animate__animated animate__fadeInUp');
+			},300);
+		}
+		if($(window).scrollTop() > 900 && window.matchMedia('(min-width: 1200px)').matches){
+			$('#service .size-headband').css('visibility', 'visible');
+			$('#service .size-headband').addClass('animate__animated animate__fadeInUp');
+		}
+		if($(window).scrollTop() > 1400 && window.matchMedia('(min-width: 1200px)').matches){
+			$('.scale-img').css('visibility', 'visible');
+			$('.scale-img').addClass('animate__animated animate__fadeInUp');
+		
+			window.setTimeout(function(){
+				$('.card-img-overlay').css('visibility', 'visible');
+				$('.card-img-overlay').addClass('animate__animated animate__fadeInUp');
+			},200);	
+		}
+		if($(window).scrollTop() > 2300 && window.matchMedia('(min-width: 1200px)').matches){
+			$('.size-headband2').css('visibility', 'visible');
+			$('.size-headband2').addClass('animate__animated animate__fadeInUp');
+		}
+	})
+})
